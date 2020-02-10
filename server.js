d77867450cfb5c31d9995a5275628b2e18d19414
madeleinewoodbury <randine25@gmail.com>
2020-02-10 15:23:00 -0600

    Setup database with mongoose

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Load routes
 const users = require('./routes/users');
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/users', users);
@@ -19,4 +29,12 @@ const server = app.listen(PORT, () => {
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta
       .bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.bgRed);
+
+  // Close server and exit process
+  server.close(() => process.exit(1));
 });
